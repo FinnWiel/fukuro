eea5f7bab2ba90dd2ff8cc4efc352c05095282d9
Roccosengers <roccosengers@live.nl>
2026-08-26 16:56:22 +0200

    Match Audible editions already in library

--- app/src/main/java/fukuro/Models.kt
+++ app/src/main/java/fukuro/Models.kt
@@ -137,6 +137,7 @@ data class Metadata(
     val publishedYear: String? = null,
     val genres: List<String> = emptyList(),
     val isbn: String? = null,
+    val asin: String? = null,
 )
 
 @Serializable
