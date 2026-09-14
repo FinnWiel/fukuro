61ca324073cf672c801157368c6227ea105253a5
FinnWiel <finnvandewiel01@gmail.com>
2026-09-14 10:29:18 +0200

    Bump version to 1.11.18

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -16,7 +16,7 @@ val buildNumber: Int = try {
     0
 }
 
-val fallbackVersionName = "1.11.17"
+val fallbackVersionName = "1.11.18"
 val releaseTagVersion = providers.environmentVariable("GITHUB_REF_NAME").orNull
     ?.takeIf { it.matches(Regex("""v\d+(\.\d+)*""")) }
     ?.removePrefix("v")
